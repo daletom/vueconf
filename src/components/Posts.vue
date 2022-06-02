d6fc9948f1c0451dcff114036dac440de294c8de
Tom Dale <thomas.dale2@gmail.com>
2022-06-02 11:45:03 -0700

    update header and sizing of posts

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -5,14 +5,10 @@
       <div class="bg-white h-1/3 sm:h-2/3" />
     </div>
     <div class="relative max-w-7xl mx-auto">
-      <div class="text-center">
-        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">From the blog</h2>
-        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.</p>
-      </div>
-      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none">
         <div v-for="post in posts" :key="post.title" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
           <div class="flex-shrink-0">
-            <img class="h-48 w-full object-cover" :src="post.imageUrl" alt="" />
+            <img class="h-72 w-full object-cover" :src="post.imageUrl" alt="" />
           </div>
           <div class="flex-1 bg-white p-6 flex flex-col justify-between">
             <div class="flex-1">
